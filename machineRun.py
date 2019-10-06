046c6639c45b2b7f87a92347273040722507f301
Samarth Shah <samarthcodes3.14@gmail.com>
2019-10-06 11:16:55 -0700

    Update machineRun.py

--- machineRun.py
+++ machineRun.py
@@ -2,7 +2,7 @@ txt = input("Type the name of the dataset you would like to use to test this out
 
 
 
-def runMachine():
+def runMachine(txt):
 
     import pandas as pd
     import numpy as np
@@ -15,13 +15,16 @@ def runMachine():
     # print(exo_data.describe())
 
     exo_X = exo_data.iloc[:, :-1].values
-    exo_Y = exo_data.iloc[:, 1].values
+    exo_Y = exo_data.iloc[:, -1].values
 
     from sklearn.model_selection import train_test_split
     X_Train, X_Test, Y_Train, Y_Test = train_test_split(exo_X, exo_Y, test_size = 1/3, random_state = 0)
 
+
+
+
     from sklearn.linear_model import LinearRegression
-    regressor = LinearRegression()
+    regressor = LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial')
     regressor.fit(X_Train, Y_Train)
 
     Y_Pred = regressor.predict(X_Test)
@@ -29,6 +32,7 @@ def runMachine():
     num_correct = 0
     for i in range(len(Y_Pred)):
         if Y_Pred[i] == Y_Test[i]:
+            print(Y_Pred[i], Y_Test[i])
             num_correct = num_correct+1
     
     print("The fraction of correctly classified examples in the test set is: " + str(num_correct / len(Y_Pred)))
@@ -42,6 +46,6 @@ def runMachine():
 
 
 
-runMachine()
+runMachine(txt)
 
 
